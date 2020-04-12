2930405faedcd1e55e3e339e4c7472b79d44bbd8
Chris Buckley <62516125+chrisbuckleycode@users.noreply.github.com>
2020-04-12 23:26:41 +0200

    modified show_rss_on_page.py
    Added support in rss_urls.txt for comments ('#') as H2 headers and for blank lines.

--- show_rss_on_page.py
+++ show_rss_on_page.py
@@ -15,12 +15,14 @@ def show_feed(feedurl, article_count):
     feed_title = d.feed.title
     feed_url = d.feed.title_detail.base
 
-    print("<p><h2>" + feed_title + "</h2></p>", file=f)
+    print("<p><h3>" + feed_title + "</h3></p>", file=f)
     print("<p>" + feed_url + "</p>", file=f)
     # print('RSS Feed Title is: ' + '\n' + feed_title + '\n', file=f)
     # print('RSS Feed url is: ' + '\n' + feed_url + '\n', file=f)
 
-    # limit = len(d.entries)
+    limit = len(d.entries)
+    if article_count > limit:
+        article_count = limit
     # print('Limit is: ' + '\n' + str(limit) + '\n')
 
 
@@ -57,16 +59,23 @@ lines = []
 for line in g.readlines():
     li = line.rstrip()
     if not li.startswith("#"):
-        lines.append(li)
-    
+        # if it isn't a comment:
+        if line.strip():
+            # if it isn't blank then add it:
+            lines.append(li)
+            # if it's a category marker then print it
+            if li.startswith("["):
+                print("<p><h2>Category: " + li[1:-1] + "</h2></p>", file=f)
 
 for each_url in lines:
-    show_feed(each_url, 7)
+    # if it's a category marker then ignore it before parsing the feed and printing it to file
+    if not each_url.startswith("["):
+        show_feed(each_url, 7)
 
 g.close()
 ###### END RSS FEEDS ######
 
-show_feed('https://reddit.com/user/roughtourist/m/covid.rss', 20)
+# show_feed('https://reddit.com/user/roughtourist/m/covid.rss', 20)
 
     
 print("</body></html>", file=f)
