021f038c737483ac630a8271c08ca8f1a9490b70
Chris Buckley <62516125+chrisbuckleycode@users.noreply.github.com>
2020-05-17 15:08:36 +0200

    added comments to grab_webcam.sh

--- grab_webcam.sh
+++ grab_webcam.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # https://zxq9.com/archives/795
 # this downloads a new image from a web cam every second
+# sudo apt-get install imagemagick
+# convert -resize 640x480 -delay 20 -loop 0 cam_image*.jpg myimage.gif
+
 image_url="http://blabla.com/webcamimage.jpg"
 while [ true ]
 do
