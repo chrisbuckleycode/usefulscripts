502ef74cc1073b654cea0a412ec6b3f260ccd8c6
Chris Buckley <62516125+chrisbuckleycode@users.noreply.github.com>
2020-05-22 20:24:10 +0200

    updated crop script with optional rotate and resize

--- crop_image_top.py
+++ crop_image_top.py
@@ -1,6 +1,6 @@
 # sudo -H pip3 install Pillow
 # after cropping, convert to animated gif with imagemagick:
-# convert -delay 10 -loop 0 *.jpg animated.gif
+# convert -delay 6 -loop 0 *.jpg animated.gif
 # optional to resize for sending via mobile:
 # convert animated.gif -resize 533x287 animated2.gif
 
@@ -18,5 +18,7 @@ for infile in listing:
     file_root, file_ext = os.path.splitext(infile)
     im = Image.open(path_plus_filename)
     cropped = im.crop((0, pixels_to_chop_from_top, 800, 450)) # left, upper, right, lower; image size is 800 x 450 px
+#    cropped = cropped.rotate(2.9)     # optional rotate and re-crop
+#    cropped = cropped.crop((9, 18, 791, 410)) # left, upper, right, lower; image size is 800 x 450 px
     cropped.save(path + "cropped_" + file_root + file_ext, "JPEG")
 
