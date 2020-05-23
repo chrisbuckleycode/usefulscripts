a3916a40687a541dd7253f08dc59fad404991100
Chris Buckley <62516125+chrisbuckleycode@users.noreply.github.com>
2020-05-23 14:27:32 +0200

    Added error checking to ensure frame captured is valid before writing image to disk

--- webcam_capture_long.py
+++ webcam_capture_long.py
@@ -1,9 +1,11 @@
 # sudo -H pip3 install opencv-python
+# sudo -H pip3 install numpy
 
 # This script opens a stream and saves frames every 30s some other long duration. The stream is opened and closed for each frame capture to prevent overread but does mean a new request occurs for every single frame. See other script for shorter duration captures.
 
 import cv2
 import time
+import numpy as np
 
 url = 'http://site.com/video.mjpg'
 
@@ -11,6 +13,17 @@ while True:
   capture = cv2.VideoCapture(url)
   seconds = time.time()  # time since unix epoch in seconds
   ret, frame = capture.read()
-  cv2.imwrite("Cam_Stream_Frame_" + str(seconds) + ".jpg", frame)
-  capture.release()  # prevents overread, last frame in buffer being stuck, also minimizes bandwidth usage
-  time.sleep(60)  # save frame every 60 seconds
+  height, width, channels = frame.shape
+  if (ret == True) and (height > 0) and (width > 0): # if a valid frame captured
+    cv2.imwrite("Cam_Stream_Frame_" + str(seconds) + ".jpg", frame)
+    print("created image: " + "Cam_Stream_Frame_" + str(seconds) + ".jpg")
+    capture.release()  # prevents overread, last frame in buffer being stuck, also minimizes bandwidth usage
+    time.sleep(30)  # save frame every X seconds
+  elif frame == None: # if frame empty
+    print("Empty Frame")    
+    capture.release()  # prevents overread, last frame in buffer being stuck, also minimizes bandwidth usage
+    time.sleep(10)  # try again in 10 seconds
+  else:
+    print("Read Failed") # if return value of False during capture.read
+    capture.release()  # prevents overread, last frame in buffer being stuck, also minimizes bandwidth usage
+    time.sleep(10)  # try again in 10 seconds
