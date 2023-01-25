87ef4e41b7103c9f906955d94f5dafd76203035e
Chris Buckley <62516125+chrisbuckleycode@users.noreply.github.com>
2023-01-25 22:46:27 +0100

    feat: bookmarks sort scripts

---
+++ bmarks-sort-alpha-title-only.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import os
+
+# Open and read the HTML file
+with open("bookmarks.html", "r") as file:
+    soup = BeautifulSoup(file, "html.parser")
+
+# Create a dictionary where the key is the title and the value is the corresponding URL
+bookmarks = {}
+for link in soup.find_all("a"):
+    bookmarks[link.get_text()] = link["href"]
+
+# Sort the dictionary by value (URL) in alphabetical order
+sorted_bookmarks = dict(sorted(bookmarks.items(), key=lambda item: item[1]))
+
+# Create and save an HTML page with a list of hyperlinks
+with open("sorted_bookmarks.html", "w") as file:
+    file.write("<html><body>")
+    for title, url in sorted_bookmarks.items():
+        file.write(f"<a href='{url}'>{title}</a><br>\n")
+    file.write("</body></html>")
