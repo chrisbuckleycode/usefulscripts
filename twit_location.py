423fc21650d05ab7e071fb614200255c85e254c2
Chris Buckley <62516125+chrisbuckleycode@users.noreply.github.com>
2023-01-31 22:07:39 +0100

    feat: twitter search by location script

---
+++ twit_location.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+city = input("Enter city name: ")
+
+# Pass city name to nominatim API
+# This is a free API requiring no key
+url = f"https://nominatim.openstreetmap.org/search?q={city}&format=json&limit=1"
+response = requests.get(url)
+data = json.loads(response.text)
+
+# Extract latitude and longitude from API response
+latitude = data[0]["lat"]
+longitude = data[0]["lon"]
+
+# Create Twitter search URL with latitude, longitude and 5 mile radius
+radius = 5
+twitter_url = f"https://twitter.com/search?q=geocode:{latitude},{longitude},{radius}mi"
+
+print("Twitter search URL:", twitter_url)
