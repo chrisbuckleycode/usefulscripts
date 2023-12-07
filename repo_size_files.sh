0931a9861c581cb157ba307690d641f83e61685c
Chris Buckley <62516125+chrisbuckleycode@users.noreply.github.com>
2023-12-07 11:32:18 +0800

    feat: repo file size script working and tested

---
+++ repo_size_files.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Error handling
+if [ -z "$1" ]
+then
+  echo "Error: No URL provided. Please provide a URL as the first argument."
+  exit 1
+fi
+
+# First argument is the git clone url
+url=$1
+
+# temp, to be cleaned up later
+tmp_dir=$(mktemp -d -t git-clone-XXXXXX)
+
+# Clone the URL into the temporary directory
+git clone $url $tmp_dir
+
+# List the files in order of size descending and output a table of filename and size
+echo "Filename Size"
+ls -lhS $tmp_dir | awk '{print $9, $5}' | column -t
+
+# Remove the temporary directory
+rm -rf $tmp_dir
