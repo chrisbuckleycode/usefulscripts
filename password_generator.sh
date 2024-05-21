72c2b1ef6fcc906f51c40a34c424c2e4f29bd687
CB <62516125+chrisbuckleycode@users.noreply.github.com>
2024-05-21 22:27:49 +0800

    feat: wordlist change+parameterise no. words (#36)

--- password_generator.sh
+++ password_generator.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-# Password generator to create 3-word passphrases using the EFF short wordlist.
-# Words "randomly" selected using urandom.
-# Made for demo purposes, no warranties made for security of passphrases!
-
-url="https://www.eff.org/files/2016/09/08/eff_short_wordlist_2_0.txt"
+url="https://www.eff.org/files/2016/09/08/eff_short_wordlist_1.txt"
+# Wordlist explanation: https://www.eff.org/dice
 
 dictionary_file="words.txt"
 dictionary_file_cut="words_cut.txt"
@@ -23,18 +20,27 @@ cut -f 2- "$dictionary_file" > "$dictionary_file_cut"
 # Count the number of lines in the dictionary file
 num_lines=$(wc -l < "$dictionary_file_cut")
 
+# Set the desired number of words in the passphrase - each word worth approx. 10 bits of entropy
+num_words=6
+
 # Loop to generate and print five random passphrases
 for ((i=1; i<=5; i++)); do
-    # Generate random line numbers
-    line1=$(od -An -N2 -i /dev/urandom | awk -v n="$num_lines" '{print $1 % n + 1}')
-    line2=$(od -An -N2 -i /dev/urandom | awk -v n="$num_lines" '{print $1 % n + 1}')
-    line3=$(od -An -N2 -i /dev/urandom | awk -v n="$num_lines" '{print $1 % n + 1}')
+    passphrase=""
+    for ((j=1; j<=num_words; j++)); do
+        # Generate random line number
+        line=$(od -An -N2 -i /dev/urandom | awk -v n="$num_lines" '{print $1 % n + 1}')
 
-    # Get the randomly selected words from the dictionary file
-    word1=$(sed -n "${line1}p" "$dictionary_file_cut")
-    word2=$(sed -n "${line2}p" "$dictionary_file_cut")
-    word3=$(sed -n "${line3}p" "$dictionary_file_cut")
+        # Get the randomly selected word from the dictionary file
+        word=$(sed -n "${line}p" "$dictionary_file_cut")
 
-    # Print the randomly selected words separated by hyphens
-    echo "Passphrase $i: $word1-$word2-$word3"
+        # Append the word to the passphrase
+        if [ "$j" -eq 1 ]; then
+            passphrase="$word"
+        else
+            passphrase="$passphrase-$word"
+        fi
+    done
+
+    # Print the passphrase
+    echo "Passphrase $i: $passphrase"
 done
