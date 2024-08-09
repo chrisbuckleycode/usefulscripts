85f9b7fcb599cef096933047efbdab48b9d36159
Chris Buckley <62516125+chrisbuckleycode@users.noreply.github.com>
2024-08-09 14:11:31 +0800

    docs: added docstring

--- shell/random_digits.sh
+++ shell/random_digits.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+##
+## FILE: random_digits.sh
+##
+## DESCRIPTION: Randomly generates 3-digit numbers (10 instances each run).
+##
+## AUTHOR: Chris Buckley (github.com/chrisbuckleycode)
+##
+## USAGE: random_digits.sh
+##
 
 # Loop to print 10 random numbers
 for ((i=0; i<10; i++)); do
