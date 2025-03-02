04a488e5865bf22a24df4270bda4407e824a6cbb
CB <62516125+chrisbuckleycode@users.noreply.github.com>
2025-03-02 19:15:43 +0800

    feat: upgrade recent.sh to 20 files (#67)

--- 1.shell/recent.sh
+++ 1.shell/recent.sh
@@ -9,8 +9,8 @@
 ## USAGE: Copy to ~/.local/bin or elsewhere in your PATH and rename to "recent".
 ##
 
-# Find the 9 most recently modified .txt files, excluding hidden dirs
-readarray -t files < <(find "$HOME" -type f -name "*.txt" -not -path "*/\.*/*" -printf "%T@ %p\n" 2>/dev/null | sort -nr | head -n 9 | cut -d' ' -f2-)
+# Find the 20 most recently modified .txt files, excluding hidden dirs
+readarray -t files < <(find "$HOME" -type f -name "*.txt" -not -path "*/\.*/*" -printf "%T@ %p\n" 2>/dev/null | sort -nr | head -n 20 | cut -d' ' -f2-)
 
 # Number and display files relative to home directory
 for i in "${!files[@]}"; do
@@ -22,7 +22,7 @@ done
 read -p "Enter file number to open (1-${#files[@]}): " num
 
 # Validate input
-if [[ "$num" =~ ^[1-9]$ ]] && [ "$num" -le "${#files[@]}" ]; then
+if [[ "$num" =~ ^([1-9]|1[0-9]|20)$ ]] && [ "$num" -le "${#files[@]}" ]; then
     # Open file in xed without waiting
     xed "${files[$((num-1))]}" &
 else
